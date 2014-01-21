ed9c2c51cbed1d6447dafa0d4fcf6407fb354f10
Hack Reactor Students <david.mansy@icloud.com>
2014-01-20 19:52:46 -0800

    can add rooms dynamically

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,13 +4,16 @@ $(document).ready(function() {
   var contentDiv = $(".messages");
   var lastMessageTime = new Date("2000-01-21T00:12:59.906Z");
   var username = window.location.search.slice(10);
-  console.log(username);
+  var currentRoom = "green room";
+  var rooms = {};
+  var $roomsAvail = $('.roomsAvail');
+
 
   var makeMessage = function() {
     var message = {};
     message.username = username;
     message.text = $(".messagebox").val();
-    message.room = 'test';
+    message.room = currentRoom;
     return message;
   };
 
@@ -27,35 +30,64 @@ $(document).ready(function() {
         console.log("Error with message: " + error.message);
       }
     });
-
   };
 
 
-  var fetch = function(fn) {
+  var getMessages = function() {
+    var promise = $.Deferred();
     $.ajax({
       url: "https://api.parse.com/1/classes/chatterbox",
       dataType:'json',
       contentType:'application/json',
       data: {'order':'-createdAt'},
       success: function(data) {
-        var messages = data.results;
-        $.each(messages, function(index, message) {
-          if(new Date(message.createdAt) > lastMessageTime) {
-            fn(message);
-            lastMessageTime = new Date(message.createdAt);
-          }
-        });
-
-      },
-      error: function(error) {
-        console.log("Error: " + error.message);
+        promise.resolve(data.results);
       }
+    });
+    return promise;
+  };
+
+  var storeRooms = function(message) {
+    if(message.room && !(rooms[message.room])) {
+      rooms[message.room] = message.room;
+      addRoomsToList(message.room);
+      console.log(message.room);
+    }
+  };
+
+  var addRoomsToList = function(room) {
+    var $option = $("<option></option>");
+    $option.attr("value", room);
+    $option.text(room);
+    $roomsAvail.append($option);
+  };
+
+  var fetch = function(fn) {
+    var messagesPromise = getMessages();
+    messagesPromise.done(function(messages) {
+      $.each(messages, function(index, message) {
+        if(new Date(message.createdAt) > lastMessageTime) {
+          fn(message);
+          lastMessageTime = new Date(message.createdAt);
+          storeRooms(message);
+        }
+      });
+    });
+  };
+
+  var firstFetch = function(fn) {
+    var messagesPromise = getMessages();
+    messagesPromise.done(function(messages) {
+      $.each(messages, function(index, message) {
+        fn(message);
+        storeRooms(message);
+      });
     });
   };
 
   var display = function(message) {
     var messageDiv = $("<div></div>").text(message.username + ' said ' + message.text +
-                                            ', created at: ' + new Date(message.createdAt));
+                                            ', created at: ' + new Date(message.createdAt) + ' from room: ' + message.room);
     contentDiv.prepend(messageDiv);
   };
 
@@ -69,7 +101,28 @@ $(document).ready(function() {
     send(makeMessage());
     $('.messagebox').val("");
   });
-
+  firstFetch(display);
   refresh();
 
 });
+  // var fetch = function(fn) {
+  //   $.ajax({
+  //     url: "https://api.parse.com/1/classes/chatterbox",
+  //     dataType:'json',
+  //     contentType:'application/json',
+  //     data: {'order':'-createdAt'},
+  //     success: function(data) {
+  //       var messages = data.results;
+  //       $.each(messages, function(index, message) {
+  //         if(new Date(message.createdAt) > lastMessageTime) {
+  //           fn(message);
+  //           lastMessageTime = new Date(message.createdAt);
+  //         }
+  //       });
+
+  //     },
+  //     error: function(error) {
+  //       console.log("Error: " + error.message);
+  //     }
+  //   });
+  // };
